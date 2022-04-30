1565d7f77f3bf6011004aaa6f3205ccb885df3c8
Naciye Öngün <naciyeongun@hotmail.com>
2022-04-30 18:42:11 +0300

    update

--- Game Cleaner/Assets/Scripts/move.cs
+++ Game Cleaner/Assets/Scripts/move.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
+    [SerializeField] float speed;
+    [SerializeField] float moved;
+    [SerializeField] float jump;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent < Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0, 0, -speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate( moved * Time.deltaTime,0,0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(-moved * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(moved * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(-moved * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(0,jump * Time.deltaTime, 0);
+           // rb.AddForce(new Vector3(0, jump, 0));
+        }
     }
 }
